b0d5737ef61f411acbce2db38de1d9908cc02788
Vardan <vardans419@gmail.com>
2025-08-06 01:49:09 +0530

    model pushing done

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -30,3 +30,16 @@ class ModelTrainerArtifact:
     metric_artifact:ClassificationMetricArtifact    
 
 
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted:bool
+    changed_accuracy:float
+    s3_model_path:str 
+    trained_model_path:str
+
+@dataclass
+class ModelPusherArtifact:
+    bucket_name:str
+    s3_model_path:str    
+
+
